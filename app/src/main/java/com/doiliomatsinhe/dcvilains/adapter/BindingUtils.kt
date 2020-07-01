93b0eaf6d8d78d134489164022453ce4d58f7e4d
doilio <doiliomatsinhe@hotmail.com>
2020-07-01 22:12:51 +0200

    Add CrossFade
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- app/src/main/java/com/doiliomatsinhe/dcvilains/adapter/BindingUtils.kt
+++ app/src/main/java/com/doiliomatsinhe/dcvilains/adapter/BindingUtils.kt
@@ -1,6 +1,7 @@
 package com.doiliomatsinhe.dcvilains.adapter
 
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -16,20 +17,22 @@ import com.doiliomatsinhe.dcvilains.model.Villain
 fun ImageView.setVillainImage(item: Villain, cardView: CardView) {
 
     this.load(item.images.md) {
-        transformations(object : Transformation {
+
+      transformations(object : Transformation {
             override fun key() = "dowyTransformation"
 
-            override suspend fun transform(
-                pool: BitmapPool,
-                input: Bitmap,
-                size: Size
-            ): Bitmap {
+            override suspend fun transform(pool: BitmapPool, input: Bitmap, size: Size): Bitmap {
                 val p = Palette.from(input).generate()
-                cardView.setCardBackgroundColor(p.getDarkMutedColor(-0xcccccd))
+                if (p.darkMutedSwatch != null){
+                    cardView.setCardBackgroundColor(p.getDarkMutedColor(0))
+                }else{
+                    cardView.setCardBackgroundColor(Color.DKGRAY)
+                }
 
                 return input
             }
-        })
+        }).crossfade(true)
+
     }
 }
 
