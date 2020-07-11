64201d24a62b130bdf8e3642672ffa1b9201ffef
doilio <doiliomatsinhe@hotmail.com>
2020-07-11 12:19:41 +0200

    Populate the rest of DetailFragment
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- app/src/main/java/com/doiliomatsinhe/dcvilains/ui/villaindetail/VillainDetailFragment.kt
+++ app/src/main/java/com/doiliomatsinhe/dcvilains/ui/villaindetail/VillainDetailFragment.kt
@@ -63,24 +63,108 @@ class VillainDetailFragment : Fragment() {
         val appearance = villain.appearance
         val villainRace = when (appearance.race) {
             null -> " with unknown race"
-            else->" ${appearance.race}"
+            else -> " ${appearance.race}"
         }
         val villainHair = when (appearance.hairColor) {
             "-" -> "."
             "No Hair" -> " and ${appearance.hairColor}."
-            else->" and ${appearance.hairColor} hair."
+            else -> " and ${appearance.hairColor} hair."
         }
         val villainEyeColor = when (appearance.eyeColor) {
             "-" -> ""
-            else->", has ${appearance.eyeColor} eyes"
+            else -> ", has ${appearance.eyeColor} eyes"
         }
 
-        val appearanceText = "${villain.name} is a ${appearance.gender}$villainRace, who is ${appearance.height[1]} tall, weights ${appearance.weight[1]}$villainEyeColor$villainHair"
+        val appearanceText =
+            "${villain.name} is a ${appearance.gender}$villainRace, who is ${appearance.height[1]} tall, weights ${appearance.weight[1]}$villainEyeColor$villainHair"
         binding.textAppearance.text = appearanceText
 
-        // TODO Populate Biography
-        // TODO Populate Affiliations
-        // TODO Populate Relatives
+        //  Populate Biography
+        val biography = villain.biography
+        val work = villain.work
+
+        val fullName = when (biography.fullName) {
+            "" -> "Fullname is unknown.\n"
+            else -> "Fullname is ${biography.fullName}.\n"
+        }
+        val placeOfBirth = when (biography.placeOfBirth) {
+            "-" -> "Place of birth is unknown.\n"
+            else -> "Born in ${biography.placeOfBirth}.\n"
+        }
+
+        val aliasesText: String
+        if (biography.aliases[0] == "-") {
+            aliasesText = ""
+        } else {
+            var aliases = ""
+            for (name in biography.aliases) {
+                aliases += "* $name\n"
+            }
+            aliasesText = "Usually goes by:\n$aliases\n"
+        }
+
+        val occupation = when (work.occupation) {
+            "-" -> "Unknown\n"
+            else -> "${work.occupation}\n"
+
+        }
+
+        val base = when (work.base) {
+            "-" -> "Unknown"
+            else -> work.base
+
+        }
+
+        val biographyText =
+            "$fullName$placeOfBirth${aliasesText}First appeared in ${biography.firstAppearance} published by ${biography.publisher}.\nOcupation: ${occupation}Based at: $base".trim()
+        binding.textBiography.text = biographyText
+
+        // Populate Affiliations
+
+        val connections = villain.connections
+
+        val affiliationsText = when (connections.groupAffiliation) {
+            "-" -> "No known affiliations."
+            else -> {
+
+                var result = ""
+                if (connections.groupAffiliation.contains("; ")) {
+                    for (affiliation in connections.groupAffiliation.split("; ")) {
+                        result += "* $affiliation\n"
+                    }
+                } else {
+                    for (affiliation in connections.groupAffiliation.split(", ")) {
+                        result += "* $affiliation\n"
+                    }
+                }
+                "Affiliated with: \n$result".trim()
+            }
+        }
+
+        binding.textAffiliations.text = affiliationsText
+
+        // Populate Relatives
+
+        val relativesText = when (connections.relatives) {
+            "-" -> "No known relatives.\n"
+            else -> {
+
+                var result = ""
+                if (connections.relatives.contains("; ")) {
+                    for (relative in connections.relatives.split("; ")) {
+                        result += "* $relative\n"
+                    }
+                } else {
+                    for (relative in connections.relatives.split(", ")) {
+                        result += "* $relative\n"
+                    }
+                }
+                result
+            }
+        }
+
+        binding.textRelatives.text = relativesText
+
     }
 
 
@@ -96,6 +180,15 @@ class VillainDetailFragment : Fragment() {
                 arguments.cardColor, 0.50f
             )
         }
+
+        binding.apply {
+            titleAffiliations.setTextColor(arguments.cardColor)
+            titleAppearance.setTextColor(arguments.cardColor)
+            titleBiography.setTextColor(arguments.cardColor)
+            titlePowerStats.setTextColor(arguments.cardColor)
+            titleRelatives.setTextColor(arguments.cardColor)
+        }
+
     }
 
     private fun initComponents() {
